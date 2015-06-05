c80b730954dbee2a305556bfc5fe9e12afd5ca08
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-06-04 19:06:26 -0500

    map icons with click event

--- app/controllers/warnings_controller.rb
+++ app/controllers/warnings_controller.rb
@@ -1,2 +1,14 @@
 class WarningsController < ApplicationController
+
+  def create
+    warning = Warning.new(warning_params)
+    warning.save!
+    render json: warning
+  end
+
+
+  def warning_params
+      params.require(:warning).permit(:date_added, :warning_type)
+  end
+
 end
--- app/models/warning.rb
+++ app/models/warning.rb
@@ -2,5 +2,10 @@ class Warning < ActiveRecord::Base
   belongs_to :bikerack
   has_one :comment
 
-  enum warning_type: [ :pothole, :bandit, :construction, :crowded, :ok ]
+  def
+  end
+
+  def method_name
+
+  end
 end
